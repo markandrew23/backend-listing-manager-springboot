721ab89ba490150883c1b0129fc3cf6bd830031f
mark <mark.andrew@agilitas.in>
2024-10-25 15:54:34 +0530

    1st commit

---
+++ src/main/java/org/example/listingmanager/ListingManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.listingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListingManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/listingmanager/ListingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.listingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListingManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
